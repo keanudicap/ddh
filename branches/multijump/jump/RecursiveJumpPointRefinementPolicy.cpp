232e5e7ce039e851f0b36d59f7c4d8adb9e1e049
dharabor <dharabor@87542584-4514-11de-9a4c-a76526a193c6>
2012-02-28 08:52:29 +0000

    Some bugfixes added. I think. This code will soon be moved elsewhere. Committing only to backup.

--- branches/multijump/jump/RecursiveJumpPointRefinementPolicy.cpp
+++ branches/multijump/jump/RecursiveJumpPointRefinementPolicy.cpp
@@ -1,51 +1,39 @@
 #include "RecursiveJumpPointRefinementPolicy.h"
 
+#include "OctileDistanceRefinementPolicy.h"
+#include "ProblemInstance.h"
 #include "RecursiveJumpPointExpansionPolicy.h"
 
+#include "mapAbstraction.h"
 #include "path.h"
 
 #include <assert.h>
 
 RecursiveJumpPointRefinementPolicy::RecursiveJumpPointRefinementPolicy(
-		JumpPointLocator* jpl) 
+		const RecursiveJumpPointExpansionPolicy* _expander) : expander(_expander)
+
 	: RefinementPolicy()
 {
-	expander = new RecursiveJumpPointExpansionPolicy(jpl);
 }
 
 RecursiveJumpPointRefinementPolicy::~RecursiveJumpPointRefinementPolicy()
 {
-	delete expander;
 }
 
 // Refine a path computed by the recursive multi-step jumping procedure.
-// This operation involves re-expanding every node along the path and
-// inserting every intermediate jump point which was jumped over 
-// (i.e. those jump points with a branching factor of 1) 
+// This operation involves inserting into the final path every intermediate 
+// jump point which was skipped because its branching factor was < 2. 
 path* 
 RecursiveJumpPointRefinementPolicy::refine(path* abspath)
 {
 	path* thepath=abspath;
-	int maxdepth = expander->getMaxDepth();
-	node** intermediates = new node*[maxdepth];
 	while(thepath && thepath->next)
 	{
-		// expand thepath->n 
-		expander->expand(thepath->n);
-
-		// iterater over all neighbours until we find thepath->next->n
-		for( 	node* succ = expander->first(); 
-				expander->hasNext(); 
-				expander->next()	)
-		{
-			if(&*succ == &*(thepath->next->n))
-				break;	
-		}
-		assert(expander->n());
-
 		// get the intermediate nodes that were skipped on the way to
 		// thepath->next->n because their branching factor equaled 1.
 		JumpInfo* info = expander->getJumpInfo();
+		assert(info);
+		//info->print(std::cout);
 
 		// insert each intermediate jump point into the path
 		// (the last node == thepath->n, so skip it)
@@ -55,7 +43,6 @@ RecursiveJumpPointRefinementPolicy::refine(path* abspath)
 			thepath->next = newnext;
 			thepath = newnext;
 		}
+		thepath = thepath->next;
 	}
-	delete intermediates;
-	return abspath;
 }
