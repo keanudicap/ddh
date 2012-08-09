8458b6f38522e20843bd31d2d275e6b6a87adc98
dharabor <dharabor@87542584-4514-11de-9a4c-a76526a193c6>
2012-08-09 09:57:17 +0000

    forgot to add util directory + constants.h. also, moving gm_parser here

---
+++ branches/warthog/util/constants.h
@@ -0,0 +1,16 @@
+#ifndef WARTHOG_CONSTANTS_H
+#define WARTHOG_CONSTANTS_H
+
+#include <cmath>
+#include <climits>
+
+namespace warthog
+{
+	typedef unsigned char dbword;
+	const int DBWORD_BITS = sizeof(warthog::dbword)*8;
+	const int LOG2_DBWORD_BITS = ceil(log10(warthog::DBWORD_BITS) / log10(2));
+}
+
+#endif
+
+
