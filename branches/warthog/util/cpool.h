2bb9b22524386b67417a2a994a71d93ba6fe1b4d
dharabor <dharabor@87542584-4514-11de-9a4c-a76526a193c6>
2012-08-28 04:36:22 +0000

    added ::current_chunk_ and rolled back caching chunk headers (seems to make no difference)

--- branches/warthog/util/cpool.h
+++ branches/warthog/util/cpool.h
@@ -78,27 +78,15 @@ class cchunk
 		deallocate(char* addr)
 		{
 			#ifndef NDEBUG
-			if(addr < mem_ || (addr+obj_size_) > max_addr_)
+			if((addr-mem_) >= pool_size_)
 			{
 				std::cerr << "err; warthog::mem::cchunk; freeing memory outside"
 					" range of the chunk at addr: "<<&mem_ << "\n";
-			}
-
-			if((addr + obj_size_) > next_addr_)
-			{
-				std::cerr << "err; warthog::mem::cchunk freeing "
-					<< "unallocated memory in chunk at addr "<<&mem_<<"\n";
 			}
 			#endif
 
 			freed_stack_[stack_size_] = addr - mem_;
 			stack_size_++;
-			//if(stack_size_*obj_size_ == pool_size_)
-			//{
-			//	// mark the entire chunk as free
-			//	next_addr_ = mem_;
-			//	stack_size_ = 0;
-			//}
 		}
 
 		inline bool
@@ -111,19 +99,19 @@ class cchunk
 			return false;
 		}
 
-		char* 
+		inline char* 
 		first_addr()
 		{
 			return mem_;
 		}
 
-		size_t
+		inline size_t
 		pool_size()
 		{
 			return pool_size_;
 		}
 		
-		size_t
+		inline size_t
 		mem()
 		{
 			size_t bytes = sizeof(*this);
@@ -159,13 +147,11 @@ class cpool
 		   	num_chunks_(0), max_chunks_(2), obj_size_(obj_size)
 		{
 			chunks_ = new cchunk*[max_chunks_];
-			chunk_faddr_ = new char*[max_chunks_];
-			chunk_size_ = new size_t[max_chunks_];
-
 			for(int i = 0; i < (int) max_chunks_; i++)
 			{
 				add_chunk(warthog::mem::DEFAULT_CHUNK_SIZE);
 			}
+			current_chunk_ = 0;
 		}
 
 		~cpool()
@@ -175,29 +161,32 @@ class cpool
 				delete chunks_[i];
 			}
 			delete [] chunks_;
-			delete [] chunk_faddr_;
-			delete [] chunk_size_;
 		}
 
 		inline char*
 		allocate()
 		{
-			// look for space in an existing chunk
-			// NB: linear-time search! increase DEFAULT_CHUNK_SIZE if
-			// number of chunks grows too large
-			char* mem_ptr = 0;
-			for(unsigned int i=0; i < num_chunks_; i++)
+			char* mem_ptr = chunks_[current_chunk_]->allocate();
+			if(!mem_ptr)
 			{
-				mem_ptr = chunks_[i]->allocate();
-				if(mem_ptr)
+				// look for space in an existing chunk
+				// NB: linear-time search! increase DEFAULT_CHUNK_SIZE if
+				// number of chunks grows too large
+				for(unsigned int i=0; i < num_chunks_; i++)
 				{
-					return mem_ptr;
+					mem_ptr = chunks_[i]->allocate();
+					if(mem_ptr)
+					{
+						current_chunk_ = i;
+						return mem_ptr;
+					}
 				}
-			}
 
-			// not enough space in any existing chunk; make a new one
-			add_chunk(warthog::mem::DEFAULT_CHUNK_SIZE);
-			mem_ptr = chunks_[num_chunks_-1]->allocate();
+				// not enough space in any existing chunk; make a new one
+				add_chunk(warthog::mem::DEFAULT_CHUNK_SIZE);
+				current_chunk_ = num_chunks_-1;
+				mem_ptr = chunks_[current_chunk_]->allocate();
+			}
 			return mem_ptr;
 		}
 
@@ -206,18 +195,16 @@ class cpool
 		{
 			for(unsigned int i=0; i < num_chunks_; i++)
 			{
-				// TODO: this loop involves jumping through memory in 
-				// poolsize steps and doing comparisons against address. 
-				// it would be better if the values of these address were 
-				// part of a header struct which could all be initialised 
-				// and stored separately.
-				if((addr - chunk_faddr_[i]) < warthog::mem::DEFAULT_CHUNK_SIZE);
+				if((addr-chunks_[i]->first_addr()) < chunks_[i]->pool_size())
 				{
 					chunks_[i]->deallocate(addr);
+					return;
 				}
 			}
+			#ifndef NDEBUG
 			std::cerr << "err; cpool::free "
 				"tried to free an address not in any chunk!\n";
+			#endif
 		}
 
 		size_t
@@ -229,8 +216,6 @@ class cpool
 				bytes += chunks_[i]->mem();
 			}
 			bytes += sizeof(warthog::mem::cchunk*)*max_chunks_;
-			bytes += sizeof(chunk_faddr_)*max_chunks_;
-			bytes += sizeof(chunk_size_)*max_chunks_;
 			bytes += sizeof(*this);
 			return bytes;
 		}
@@ -250,8 +235,7 @@ class cpool
 
 	private:
 		warthog::mem::cchunk** chunks_;
-		char** chunk_faddr_; // first addr of each chunk
-		size_t* chunk_size_; // size of each chunk
+		size_t current_chunk_;
 
 		size_t num_chunks_;
 		size_t max_chunks_;
@@ -268,8 +252,6 @@ class cpool
 			if(num_chunks_ < max_chunks_)
 			{
 				chunks_[num_chunks_] = new cchunk(obj_size_, pool_size);
-				chunk_faddr_[num_chunks_] = chunks_[num_chunks_]->first_addr();
-				chunk_size_[num_chunks_] = chunks_[num_chunks_]->pool_size();
 				num_chunks_++;
 			}
 			else
@@ -277,27 +259,17 @@ class cpool
 				// make room for a new chunk
 				size_t big_max= max_chunks_*2;
 				cchunk** big_chunks = new cchunk*[big_max];
-				char** big_chunk_faddr = new char*[big_max];
-				size_t* big_chunk_size = new size_t[big_max];
 				for(unsigned int i = 0; i < max_chunks_; i++)
 				{
 					big_chunks[i] = chunks_[i];
-					big_chunk_faddr[i] = chunk_faddr_[i];
-					big_chunk_size[i] = chunk_size_[i];
 				}
 				delete [] chunks_;
-				delete [] chunk_faddr_;
-				delete [] chunk_size_;
 
 				chunks_ = big_chunks;
-				chunk_faddr_ = big_chunk_faddr;
-				chunk_size_ = big_chunk_size;
 				max_chunks_ = big_max;
 
 				// finally; add a new chunk
 				chunks_[num_chunks_] = new cchunk(obj_size_, pool_size);
-				chunk_faddr_[num_chunks_] = chunks_[num_chunks_]->first_addr();
-				chunk_size_[num_chunks_] = chunks_[num_chunks_]->pool_size();
 				num_chunks_++;
 			}
 		}
