a74be1815a53638714158eb30d893ab03ae97b6f
dharabor <dharabor@87542584-4514-11de-9a4c-a76526a193c6>
2012-09-03 03:46:11 +0000

    some code that tries to reduce cache misses during search. results are marginal and overall performance is slighty reduced. adding it here for future reference only.

---
+++ experimental/blocklist.h
@@ -0,0 +1,59 @@
+#ifndef WARTHOG_BLOCKLIST_H
+#define WARTHOG_BLOCKLIST_H
+
+// blocklist.h
+//
+// A heap for allocated warthog::search_node objects.
+// Stores addresses of allocated nodes and also acts as
+// a closed list of sorts.
+// This implementation is specialised for spatial networks.
+// Addresses can be stored in one of several buckets where each
+// bucket represents a cell in a square subdivision of the 
+// spatial net. 
+// The idea here is to improve performance when looking up the
+// addresses of nearby nodes. For example, two nodes may
+// which may be in close spatial proximity should also have
+// their addresses stored in close proximity in poolory.
+// This is not the case when using a simple 2d flattened
+// array.
+//
+// @author: dharabor
+// @created: 02/09/2012
+//
+
+#include "cpool.h"
+#include "search_node.h"
+
+#include <stdint.h>
+
+namespace warthog
+{
+
+class blocklist
+{
+	public:
+		blocklist(uint32_t mapwidth, uint32_t mapheight);
+		~blocklist();
+
+		// return a warthog::search_node object corresponding to the given id.
+		// if the node has already been generated, return a pointer to the 
+		// previous instance; otherwise allocate memory for a new object.
+		warthog::search_node*
+		generate(uint32_t node_id);
+
+		void
+		clear();
+
+		uint32_t
+		mem();
+
+	private:
+		uint32_t num_blocks_;
+		warthog::search_node*** blocks_;
+		warthog::mem::cpool* pool_;
+};
+
+}
+
+#endif
+
